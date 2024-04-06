9f129e0783f95d72f22d5b1fd0ba9525f44227c7
Mario <mario300401@gmail.com>
2024-04-06 15:02:40 -0300

    Se agrega el ejercicio 3 al proyecto

---
+++ tp1_MarioRios/src/ar/edu/unju/fi/ejercicio3/Main.java
@@ -0,0 +1,23 @@
+package ar.edu.unju.fi.ejercicio3;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Declarar el Scanner y la variable
+		Scanner sc = new Scanner(System.in);
+		int numAlmacenado;
+		
+		numAlmacenado = sc.nextInt();
+		
+		// Condición y Resultados
+		if (numAlmacenado % 2 == 0) {
+			System.out.println(numAlmacenado+" es un número par. Su triple es: "+numAlmacenado*3);
+		} else {
+			System.out.println(numAlmacenado+" es un número impar. Su doble es "+numAlmacenado*2);
+		}
+
+	}
+}
