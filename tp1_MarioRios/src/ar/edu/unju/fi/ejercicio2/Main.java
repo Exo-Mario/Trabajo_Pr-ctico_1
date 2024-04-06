d4e162e1c0435afb986de0edee0bfe850427188e
Mario <mario300401@gmail.com>
2024-04-06 14:43:36 -0300

    Se agrega el ejercicio 2 al proyecto

---
+++ tp1_MarioRios/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,26 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Declaración de variables
+		
+		String pais="Argentina";
+		int edad= 23;
+		float alturaEdificio = 30.2f;
+		float precio = 52.9f;
+		int telefono = 3892471;
+		double coseno = Math.cos(0.5);
+		
+		// Mostrar por pantalla
+		
+		System.out.println("Pais: "+pais);
+		System.out.println("Edad: "+edad);
+		System.out.println("Altura del edificio: "+alturaEdificio);
+		System.out.println("Precio de un producto: "+precio);
+		System.out.println("Número de telofono: "+telefono);
+		System.out.println("coseno de 0.5: "+coseno);
+	}
+
+}
