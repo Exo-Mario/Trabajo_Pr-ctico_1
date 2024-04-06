1f96cfcbd0f73e8af29509ed4364e780d1576bc1
Mario <mario300401@gmail.com>
2024-04-06 04:14:11 -0300

    Se crea el proyecto y se agrega el ejercicio 1

---
+++ tp1_MarioRios/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Comentarios
+		System.out.println("Ejercicio 1 de Programación Visual");
+		// Declaración de variables y asignación
+		
+		int numero1 = 8, numero2 = 5, numero3 = 1, numero4 = 20, numero5= 9;
+		
+		// Proceso de obtener promedio
+		double promedio = (numero1 + numero2 + numero3 + numero4 + numero5)/5.0;
+		
+		// Mostrar dato
+		System.out.println(numero1+" "+numero2+" "+numero3+" "+numero4+" "+numero5);
+		System.out.println("El promedio es: "+promedio);
+	}
+
+}
